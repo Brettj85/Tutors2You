5cc61be273f4b5cfa58a3c82f8749ea7e612a999
Jackson <Brett.Jackson@charter.com>
2019-04-10 07:52:35 -0500

    first commit

---
+++ Tutors2You/Tutors2You/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tutors2You.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
